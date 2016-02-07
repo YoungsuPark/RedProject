d78f5edfd49711b5e0f288b4c268abad48052a51
youngsuPark <nice2seeu86@gmail.com>
2016-02-07 19:44:58 +0900

    logback Test

--- red/src/main/java/net/javap/web/HomeController.java
+++ red/src/main/java/net/javap/web/HomeController.java
@@ -1,12 +1,17 @@
 package net.javap.web;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 	@RequestMapping("/")
 	public String home(){
-		return "home";
+		log.debug("Template Success");
+		return "/home/home";
 	}
 }
